4c7ce4d4c7d81d4c2088c47510f0d5cc05b6d3da
ecogle <ecogle@gmail.com>
2023-07-15 06:53:02 -0500

    added the formula

--- src/Pages/VentilatorManagement/DesiredFIO2/DesiredFIO2.js
+++ src/Pages/VentilatorManagement/DesiredFIO2/DesiredFIO2.js
@@ -31,85 +31,102 @@ function DesiredFIO2({ props }) {
   return (
     <>
       <NavBar />
-      <h2>Desired FiO2</h2>
+      <div style={{ margin: "auto" }}>
+        <h2 style={{ margin: "auto", textAlign: "center" }}>Desired FiO2</h2>
+      </div>
       <Container
-        md="5"
         style={{
-          width: "25%",
+          width: "100%",
           marginTop: "15px",
           borderRadius: "10px",
-          marginLeft: "10px",
         }}
       >
-        <Form>
-          <Row>
-            <Col>
-              <Form.Label>Desired PaO2</Form.Label>
-              <Form.Control
-                ref={dpao2Input}
-                name="dpao2"
-                id="dpao2"
-                value={dpao2}
-                onChange={(e) => setDpao2(e.target.value)}
-                type="text"
-              ></Form.Control>
-            </Col>
-          </Row>
-          <Row style={{ paddingTop: "20px" }}>
-            <Col>
-              <Form.Label>Known FiO2</Form.Label>
-              <Form.Control
-                ref={kfio2Input}
-                id="kfio2"
-                name="kfio2"
-                value={kfio2}
-                onChange={(e) => setKfio2(e.target.value)}
-                type="text"
-              ></Form.Control>
-            </Col>
-          </Row>
-          <Row style={{ paddingTop: "20px" }}>
-            <Col>
-              <Form.Label>Known PaO2</Form.Label>
-              <Form.Control
-                ref={kpao2Input}
-                name="kpao2"
-                id="kpao2"
-                value={kpao2}
-                onChange={(e) => setkpao2(e.target.value)}
-                type="text"
-              ></Form.Control>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Row style={{ paddingTop: "5px" }}>
+        <Row>
+          <Col
+            style={{
+              width: "25%",
+            }}
+          >
+            <Form>
+              <Row>
                 <Col>
-                  <Button onClick={handleSubmit} style={{ width: "100%" }}>
-                    Submit
-                  </Button>
+                  <Form.Label>Desired PaO2</Form.Label>
+                  <Form.Control
+                    ref={dpao2Input}
+                    name="dpao2"
+                    id="dpao2"
+                    value={dpao2}
+                    onChange={(e) => setDpao2(e.target.value)}
+                    type="text"
+                  ></Form.Control>
                 </Col>
+              </Row>
+              <Row style={{ paddingTop: "20px" }}>
                 <Col>
-                  <Button onClick={handleClearForm} style={{ width: "100%" }}>
-                    Clear
+                  <Form.Label>Known FiO2</Form.Label>
+                  <Form.Control
+                    ref={kfio2Input}
+                    id="kfio2"
+                    name="kfio2"
+                    value={kfio2}
+                    onChange={(e) => setKfio2(e.target.value)}
+                    type="text"
+                  ></Form.Control>
+                </Col>
+              </Row>
+              <Row style={{ paddingTop: "20px" }}>
+                <Col>
+                  <Form.Label>Known PaO2</Form.Label>
+                  <Form.Control
+                    ref={kpao2Input}
+                    name="kpao2"
+                    id="kpao2"
+                    value={kpao2}
+                    onChange={(e) => setkpao2(e.target.value)}
+                    type="text"
+                  ></Form.Control>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Row style={{ paddingTop: "5px" }}>
+                    <Col>
+                      <Button onClick={handleSubmit} style={{ width: "100%" }}>
+                        Submit
+                      </Button>
+                    </Col>
+                    <Col>
+                      <Button
+                        onClick={handleClearForm}
+                        style={{ width: "100%" }}
+                      >
+                        Clear
+                      </Button>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <hr />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Button className="btn btn-danger" style={{ width: "100%" }}>
+                    FiO2: {(dfio2 / 100).toFixed(2)}
                   </Button>
                 </Col>
               </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <hr />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button className="btn btn-danger" style={{ width: "100%" }}>
-                FiO2: {(dfio2 / 100).toFixed(2)}
-              </Button>
-            </Col>
-          </Row>
-        </Form>
+            </Form>
+          </Col>
+          <Col>
+            <h4>Formula</h4>
+            <Row>
+              <Col>DFio2 = DPaO2 * KFiO2 / KPaO2</Col>
+            </Row>
+          </Col>
+        </Row>
       </Container>
     </>
   );
