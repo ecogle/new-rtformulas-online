186b9641f74b7ff302def5ec6dfb7a61daee325d
Chad Ogle <ecogle@gmail.com>
2023-08-02 17:25:29 -0500

    made the new form functional

--- src/Pages/Formulas/OxygenContent.js
+++ src/Pages/Formulas/OxygenContent.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import PageTitle from "../../components/PageTitle/PageTitle";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import PaCO2 from "../../components/Subs/Paco2";
 import SaO2 from "../../components/Subs/SaO2";
 import CaO2 from "../../components/Subs/CaO2";
+import "../../Utilities/Utilities";
+import { ValidateNumber } from "../../Utilities/Utilities";
 
 export default function OxygenContent() {
+  const [hgb, setHgb] = useState("");
+  const [sao2, setSao2] = useState("");
+  const [paco2, setPaco2] = useState("");
+
+  const handleClear = (event) => {
+    event.preventDefault();
+    setHgb("");
+    setSao2("");
+    setPaco2("");
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <>
       <NavBar />
@@ -18,7 +33,11 @@ export default function OxygenContent() {
               <Row>
                 <Col>
                   <Form.Label>Hgb</Form.Label>
-                  <Form.Control type="input" />
+                  <Form.Control
+                    type="input"
+                    value={hgb}
+                    onChange={(e) => ValidateNumber(e.target.value, setHgb)}
+                  />
                 </Col>
               </Row>
               <Row className="row-padding">
@@ -26,7 +45,11 @@ export default function OxygenContent() {
                   <Form.Label>
                     <SaO2 />
                   </Form.Label>
-                  <Form.Control type="input" />
+                  <Form.Control
+                    type="input"
+                    value={sao2}
+                    onChange={(e) => ValidateNumber(e.target.value, setSao2)}
+                  />
                 </Col>
               </Row>
               <Row className="row-padding">
@@ -35,15 +58,23 @@ export default function OxygenContent() {
                     <PaCO2 />
                   </Form.Label>
 
-                  <Form.Control type="input" />
+                  <Form.Control
+                    type="input"
+                    value={paco2}
+                    onChange={(e) => ValidateNumber(e.target.value, setPaco2)}
+                  />
                 </Col>
               </Row>
               <Row className="row-padding">
                 <Col>
-                  <Button style={{ width: "100%" }}>Submit</Button>
+                  <Button style={{ width: "100%" }} onClick={handleSubmit}>
+                    Submit
+                  </Button>
                 </Col>
                 <Col>
-                  <Button style={{ width: "100%" }}>Clear</Button>
+                  <Button style={{ width: "100%" }} onClick={handleClear}>
+                    Clear
+                  </Button>
                 </Col>
               </Row>
               <Row className="row-padding">
